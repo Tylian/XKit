89c8199b6523e6000c8318baf8afac2190a15c76
James Hobin <hobinjk@mit.edu>
2015-07-17 02:27:38 -0700

    Implement lethe properly

--- Extensions/lethe.js
+++ Extensions/lethe.js
@@ -10,17 +10,17 @@
 XKit.extensions.lethe = new Lethe();
 
 function Lethe() {
-  this.TOP_CUTOFF = -3000;
+  this.TOP_CUTOFF = -500;
   this.running = false;
   this.scrollWaiting = false;
-  this.preferences = {};
-  //   'sep0': {
-  //     text: 'Options',
-  //     type: 'separator'
-  //   },
-  // };
-  //
-  this.handleScroll = this.handleScroll.bind(this);
+  this.hiddenPosts = [];
+  this.preferences = {
+    'sep0': {
+      text: 'Options',
+      type: 'separator'
+    }
+  };
+
 }
 
 /**
@@ -28,6 +28,7 @@ function Lethe() {
  */
 Lethe.prototype.run = function() {
   this.running = true;
+  this.handleScroll = this.handleScroll.bind(this);
   window.addEventListener('scroll', this.handleScroll, false);
 };
 
@@ -49,13 +50,13 @@ Lethe.prototype.handleScroll = function() {
 Lethe.prototype.updatePosts = function() {
   this.scrollWaiting = false;
 
-  var postMedias = document.querySelectorAll('.post_media');
+  var postMedias = document.querySelectorAll('.post_wrapper');
   var i;
   for (i = 0; i < postMedias.length; i++) {
     var postMedia = postMedias[i];
     var rect = postMedia.getBoundingClientRect();
 
-    if (rect.top + rect.height > this.TOP_CUTOFF) {
+    if (rect.bottom > this.TOP_CUTOFF) {
       // Rect is fully on screen, we want to keep it
       break;
     }
@@ -64,7 +65,7 @@ Lethe.prototype.updatePosts = function() {
 
   for (i = this.hiddenPosts.length - 1; i > -1; i--) {
     var hiddenMedia = this.hiddenPosts[i];
-    if (hiddenMedia.absoluteTop < window.scrollY + this.TOP_CUTOFF) {
+    if (hiddenMedia.absoluteBottom < window.scrollY + this.TOP_CUTOFF) {
       // hiddenMedia is higher than the current viewport
       // all other posts should also be higher
       break;
@@ -78,22 +79,21 @@ Lethe.prototype.updatePosts = function() {
  * @param {DOMElement} post
  */
 Lethe.prototype.hidePost = function(post) {
-  console.log('Hiding a post');
   var rect = post.getBoundingClientRect();
   var placeholder = document.createElement('div');
-  placeholder.classList.add('post_media_hidden');
+  placeholder.classList.add('post_wrapper_hidden');
   placeholder.style.width = rect.width + 'px';
   placeholder.style.height = rect.height + 'px';
-  post.parentNode.replaceChild(placeholder, post);
 
-  var absoluteTop = rect.top + this.scrollY;
-  if (absoluteTop < this.hiddenPosts[this.hiddenPosts.length - 1].absoluteTop) {
-    throw new Error('Lethe post ordering invariant broken');
-  }
+  var parentNode = post.parentNode;
+  parentNode.replaceChild(placeholder, post);
+
+  var absoluteBottom = rect.bottom + window.scrollY;
+
   this.hiddenPosts.push({
-    absoluteTop: absoluteTop,
-    html: post.innerHTML,
-    parent: post.parentNode
+    absoluteBottom: absoluteBottom,
+    html: post.outerHTML,
+    parent: parentNode
   });
 };
 
@@ -102,7 +102,6 @@ Lethe.prototype.hidePost = function(post) {
  * @param {DOMElement} hiddenPost
  */
 Lethe.prototype.showPost = function(hiddenPost) {
-  console.log('Showing a post');
   // Overly conservative removal logic
   this.hiddenPosts = this.hiddenPosts.filter(function(post) {
     return post !== hiddenPost;
