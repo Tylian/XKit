0db27c97baf30ca4079a5f79b7ef9caf57a48507
James Hobin <hobinjk@mit.edu>
2015-07-17 22:56:18 -0700

    Fix audio_plus

--- Extensions/audio_plus.js
+++ Extensions/audio_plus.js
@@ -1,11 +1,11 @@
 //* TITLE Audio+ **//
-//* VERSION 0.2 REV B **//
+//* VERSION 0.3.0 **//
 //* DESCRIPTION Enhancements for the Audio Player **//
 //* DEVELOPER STUDIOXENIX **//
 //* FRAME false **//
 //* BETA false **//
 
-XKit.extensions.audio_plus = new Object({
+XKit.extensions.audio_plus = {
 
 	running: false,
 
@@ -20,7 +20,7 @@ XKit.extensions.audio_plus = new Object({
 
 		if ($("#posts").length > 0) {
 			XKit.tools.init_css("audio_plus");
-			$(document).on("mousemove mousedown mouseup mouseout click", ".xkit-audio-plus-slider, .xkit-audio-plus-slider-back", XKit.extensions.audio_plus.slider_do);
+			$(document).on("mousemove mousedown mouseup mouseout click", ".xkit-audio-plus-slider", XKit.extensions.audio_plus.slider_handle_event);
 			XKit.post_listener.add("audio_plus", XKit.extensions.audio_plus.do);
 			setTimeout(function() { XKit.extensions.audio_plus.do(); }, 500);
 			$("body").append("<div id=\"xkit-audio-plus-current-player\"></div>");
@@ -29,8 +29,6 @@ XKit.extensions.audio_plus = new Object({
 
 	},
 
-	slider_mouse_down: false,
-
 	check_current: function() {
 
 		XKit.tools.add_function(function() {
@@ -38,8 +36,9 @@ XKit.extensions.audio_plus = new Object({
 			try {
 				var current_player = Tumblr.AudioPlayer.current_player_id;
 
-				var m_object = new Object();
-				m_object.defined = false;
+				var m_object = {
+					defined: false
+				};
 
 				if (typeof current_player !== "undefined") {
 					m_object.defined = true;
@@ -56,14 +55,6 @@ XKit.extensions.audio_plus = new Object({
 			}
 
 		}, true, "");
-
-		/*if (XKit.extensions.audio_plus.return_current_instance() === -1) {
-			console.log("No player.");
-		}else {
-			console.log("Player:");
-			console.log(XKit.extensions.audio_plus.return_current_instance());
-		}*/
-
 	},
 
 	return_current_instance: function() {
@@ -81,28 +72,28 @@ XKit.extensions.audio_plus = new Object({
 	},
 
 	stop_current_instance: function() {
+		var instance = XKit.extensions.audio_plus.return_current_instance();
 
-		if (XKit.extensions.audio_plus.return_current_instance() === -1) { return; }
+		if (instance === -1) { return; }
 
-		var m_object = new Object();
-		m_object.id = XKit.extensions.audio_plus.return_current_instance().id;
+		var m_object = {
+			id: instance.id
+		};
 
 		XKit.tools.add_function(function() {
-
 			var add_tags = JSON.parse(add_tag);
 			var m_object = audiojs.instances["audiojs" + add_tags.id];
 			m_object.pause();
-
 		}, true, JSON.stringify(m_object));
 
 	},
 
 	play_current_instance: function() {
+		var instance = XKit.extensions.audio_plus.return_current_instance();
 
-		if (XKit.extensions.audio_plus.return_current_instance() === -1) { return; }
+		if (instance === -1) { return; }
 
-		var m_object = new Object();
-		m_object.id = XKit.extensions.audio_plus.return_current_instance().id;
+		var m_object = {id: instance.id};
 
 		XKit.tools.add_function(function() {
 
@@ -114,93 +105,25 @@ XKit.extensions.audio_plus = new Object({
 
 	},
 
-	fucking_firefox_fix: function(obj, volume) {
-
-		var __id = $(obj).attr('id');
-		var m_object = new Object();
-		m_object.id = __id;
-		m_object.volume = volume;
-
-		XKit.tools.add_function(function() {
-
-			var add_tags = JSON.parse(add_tag);
-			var m_object = audiojs.instances[add_tags.id];
-			m_object.setVolume(add_tags.volume);
-
-		}, true, JSON.stringify(m_object));
-
-	},
-
-	acknowledge_slider_change: function(m_obj, m_x) {
-
-		var __volume = (m_x - $(m_obj).offset().left - 7) / 100;
-		if (__volume <= 0) { __volume = 0; }
-
-		$(m_obj).find(".xkit-audio-plus-slider").attr("title", parseInt(m_x - $(m_obj).offset().left - 7));
-
-		if (XKit.browser().firefox === true) {
-			// Firefox is a fucking piece of shit, and the IE of our time.
-			XKit.extensions.audio_plus.fucking_firefox_fix($(m_obj).parentsUntil('.post').find("object"), __volume);
-		} else {
-			$(m_obj).parentsUntil('.post').find("audio").get(0).volume = __volume;
-		}
-
-	},
-
-	slider_do: function(e) {
-
-		var m_x = e.pageX;
-		var m_y = e.pageY;
-
-		var m_obj = e.target;
-		var __kickback = false;
-
-		if ($(m_obj).hasClass("xkit-audio-plus-slider")) {
-			m_obj = $(m_obj).parent();
-			__kickback = true;
-		}
-
-		if (e.type === "mousedown") {
-			XKit.extensions.audio_plus.slider_mouse_down = true;
-		}
-
-		if (e.type === "mouseup" || e.type === "mouseout") {
-			if (e.type === "mouseout") {
-				var m_top = $(m_obj).offset().top;
-				var __diff = m_top - m_y;
-				if (__diff <= 0) { __diff = __diff * -1 };
-				if (__diff <= 10) { return; }
+	slider_handle_event: function(e) {
+		function firstParentWithClass(elt, cls) {
+			if (elt.classList.contains(cls)) {
+				return elt;
+			} else {
+				return firstParentWithClass(elt.parentNode, cls);
 			}
-			XKit.extensions.audio_plus.slider_mouse_down = false;
 		}
+		var slider =	e.target;
 
-		if (e.type === "click") {
-			XKit.extensions.audio_plus.slider_mouse_down = false;
-
-			var __left = m_x - $(m_obj).offset().left - 7;
-
-			if (__left + 14 >= 100) { __left = 100 - 14; }
-			if (__left <= 0) { __left = 0; }
-
-			$(m_obj).find(".xkit-audio-plus-slider").css("left", __left + "px");
-			XKit.extensions.audio_plus.acknowledge_slider_change(m_obj, m_x);
-
-		}
-
-		if (e.type === "mousemove") {
-			if (XKit.extensions.audio_plus.slider_mouse_down === false) {
-				return;
+		var parent_post = firstParentWithClass(slider, 'post');
+		var volume = slider.value;
+		if (e.type === "click" || e.type === "mousemove") {
+			slider.title = volume;
+			var audio = parent_post.querySelector("audio");
+			if (audio && audio.volume !== volume / 100) {
+				audio.volume = volume / 100;
 			}
-
-			var __left = m_x - $(m_obj).offset().left - 7;
-
-			if (__left + 14 >= 100) { __left = 100 - 14; }
-			if (__left <= 0) { __left = 0; }
-
-			$(m_obj).find(".xkit-audio-plus-slider").css("left", __left + "px");
-			XKit.extensions.audio_plus.acknowledge_slider_change(m_obj, m_x);
 		}
-
 	},
 
 	do: function() {
@@ -217,10 +140,8 @@ XKit.extensions.audio_plus = new Object({
 			// Check if hosted by Tumblr:
 			if ($(this).find(".audio_player").length === 0 || $(this).find(".audio_visualizer").length === 0) { return; }
 
-			var slider_html = 	"<div data-post-id=\"" + m_post.id + "\" class=\"xkit-audio-plus-slider-container\">" +
-							"<div class=\"xkit-audio-plus-slider-back\">" +
-								"<div data-post-id=\"" + m_post.id + "\" title=\"somepeoplelikefish\" class=\"xkit-audio-plus-slider\">&nbsp;</div>" +
-							"</div>" +
+			var slider_html =	 "<div data-post-id=\"" + m_post.id + "\" class=\"xkit-audio-plus-slider-container\">" +
+								"<input type=\"range\" value=\"100\" min=\"0\" max=\"100\" data-post-id=\"" + m_post.id + "\" title=\"somepeoplelikefish\" class=\"xkit-audio-plus-slider\"></input>" +
 						"</div>";
 
 			$(this).find(".post_media").append(slider_html);
@@ -238,4 +159,4 @@ XKit.extensions.audio_plus = new Object({
 		XKit.post_listener.remove("audio_plus");
 	}
 
-});
+};
