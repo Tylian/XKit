0d812130a4fe11edc0fe3e5d24028be44d1677fa
James Hobin <hobinjk@mit.edu>
2015-07-19 13:08:12 -0700

    Make create-extensions-dist as easy as possible

--- create-extensions-dist.sh
+++ create-extensions-dist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-git merge master -m "Merge master"
-node ./Extensions/dist/extensions.js ./Extensions ./Extensions/dist
-git add Extensions
-git commit -m "Rebuild distribution"
-git push
+git checkout gh-pages &&\
+git merge master -m "Merge master" &&\
+node ./Extensions/dist/extensions.js ./Extensions ./Extensions/dist &&\
+git add Extensions &&\
+git commit -m "Rebuild distribution" &&\
+git push &&\
+git checkout master
