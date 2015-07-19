d16c977744bdd9d4637c8dd922642e94c99e7498
James Hobin <hobinjk@mit.edu>
2015-07-19 12:49:54 -0700

    Further streamline create-dist

--- create-extensions-dist.sh
+++ create-extensions-dist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+git merge master -m "Merge master"
 node ./Extensions/dist/extensions.js ./Extensions ./Extensions/dist
 git add Extensions
-git commit
+git commit -m "Rebuild distribution"
 git push
