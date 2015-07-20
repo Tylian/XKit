ff57fe8bbe54de08a66f58778b4607fc33254c75
James Hobin <hobinjk@mit.edu>
2015-07-20 02:01:29 -0700

    Actually fix auto_tagger

--- Extensions/auto_tagger.js
+++ Extensions/auto_tagger.js
@@ -1,5 +1,5 @@
 //* TITLE Auto Tagger **//
-//* VERSION 0.6.2 **//
+//* VERSION 0.6.3 **//
 //* DESCRIPTION Tags posts automatically. **//
 //* DEVELOPER STUDIOXENIX **//
 //* DETAILS This extension allows you to automatically add tags to posts based on state (reblogged, original, queued) or post type (audio, video, etc) and keeping original tags while reblogging a post. **//
@@ -206,7 +206,7 @@ XKit.extensions.auto_tagger = new Object({
 			post_object = XKit.interface.find_post(post_id);
 		}
 
-		var m_tags = return_tags(post_object, original);
+		var m_tags = this.return_tags(post_object, original);
 
 		if (m_tags !== "") {
 			this.inject_to_window(m_tags);
