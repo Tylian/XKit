7c6774148c7d2a0bc5bdccdc4fe795cc3202c150
Bryan Tsang <bvtsang@gmail.com>
2015-07-20 22:09:54 -0700

    Add spacing between text in Tags on Sidebar extension.

--- Extensions/classic_tags.js
+++ Extensions/classic_tags.js
@@ -1,5 +1,5 @@
 //* TITLE Tags on Sidebar **//
-//* VERSION 1.3.1 **//
+//* VERSION 1.3.1 REV A **//
 //* DESCRIPTION Shows your tracked tags on your sidebar **//
 //* DEVELOPER STUDIOXENIX **//
 //* FRAME false **//
