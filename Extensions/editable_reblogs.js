42fd751481299e37c95fb237b1e8ced38ad50dcf
James Hobin <hobinjk@gmail.com>
2015-07-30 14:55:36 -0700

    Fix editable_reblogs' interaction with existing content

--- Extensions/editable_reblogs.js
+++ Extensions/editable_reblogs.js
@@ -1,5 +1,5 @@
 //* TITLE Editable Reblogs **//
-//* VERSION 1.0.1 **//
+//* VERSION 1.0.2 **//
 //* DESCRIPTION	Restores ability to edit previous reblogs of a post **//
 //* DEVELOPER dlmarquis **//
 //* FRAME false **//
@@ -11,28 +11,24 @@ XKit.extensions.editable_reblogs = new Object({
 
 	run: function() {
 		this.running = true;
-		
+
 		XKit.interface.post_window_listener.add("editable_reblogs", XKit.extensions.editable_reblogs.post_window);
-		
 	},
-	
+
 	post_window: function() {
-	    
-	    //alert("Entered post_window function!");
-	    
-	    var reblog_content = $(".reblog-tree").html();
-	    
-	    //alert(reblog_content);
-	    
-	    XKit.interface.post_window.set_content_html(reblog_content + "<br/>");
-	    
-	    //$(".reblog-tree).html("");
-	    //window.setTimeout(function() {
-	        $(".btn-remove-tree").click();
-	    //}, 1000);
-	    
-	    XKit.tools.add_css(".control-reblog-tree {display: none; }", "editable_reblogs_remove_content_tree");
-	    
+		var reblog_tree = $(".reblog-tree");
+		if (reblog_tree.length <= 0) {
+			return;
+		}
+
+		var reblog_content = reblog_tree.html();
+
+		var old_content = XKit.interface.post_window.get_content_html();
+		XKit.interface.post_window.set_content_html(reblog_content + old_content);
+
+		$(".btn-remove-tree").click();
+
+		XKit.tools.add_css(".control-reblog-tree {display: none; }", "editable_reblogs_remove_content_tree");
 	},
 
 	destroy: function() {
