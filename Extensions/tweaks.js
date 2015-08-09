5f703a432424f9c26c23b940f140ac7de3bc9593
ThePsionic <me@thepsionic.com>
2015-08-09 10:50:29 +0200

    Add tweak to hide trending badges

--- Extensions/tweaks.js
+++ Extensions/tweaks.js
@@ -1,5 +1,5 @@
 //* TITLE Tweaks **//
-//* VERSION 3.3.2 **//
+//* VERSION 3.3.3 **//
 //* DESCRIPTION Various little tweaks for your dashboard. **//
 //* DEVELOPER STUDIOXENIX **//
 //* DETAILS These are small little tweaks that allows you customize your dashboard. If you have used XKit 6, you will notice that some of the extensions have been moved here as options you can toggle. Keep in mind that some of the tweaks (the ones marked with a '*') can slow down your computer. **//
@@ -136,6 +136,11 @@ XKit.extensions.tweaks = new Object({
 		},
 		"hide_sponsored": {
 			text: "Dim sponsored posts on dashboard (not recommended)",
+			default: false,
+			value: false
+		},
+		"hide_trending": {
+			text: "Hides \"Trending\" badges at the bottom of posts",
 			default: false,
 			value: false
 		},
@@ -469,6 +474,10 @@ XKit.extensions.tweaks = new Object({
 
 		if (XKit.extensions.tweaks.preferences.hide_sponsored.value === true) {
 			XKit.extensions.tweaks.add_css(".post.sponsored_post { opacity: 0.33 !important } .post.sponsored_post:hover { opacity: 1 !important }", "xkit_tweaks_hide_sponsored");
+		}
+
+		if (XKit.extensions.tweaks.preferences.hide_trending.value === true) {
+			XKit.extensions.tweaks.add_css(".explore-trending-badge-footer { display:none !important; }", "xkit_tweaks_hide_trending");
 		}
 
 		if (XKit.extensions.tweaks.preferences.pin_avatars.value === true) {
