97a587c04051cfb313c56a97e6179a70c4aadd32
estufar <estufar@users.noreply.github.com>
2015-09-02 15:10:44 -0600

    integrated wolvan's comments

--- Extensions/old_sidebar.js
+++ Extensions/old_sidebar.js
@@ -21,33 +21,30 @@ XKit.extensions.old_sidebar = new Object({
 		XKit.tools.init_css("old_sidebar");
 		this.running = true;
 		
-		var movesidebar = true;
-		if (XKit.extensions.old_sidebar.preferences.dashonly.value === true) {
+		if (XKit.extensions.old_sidebar.preferences.dashonly.value) {
 			if (document.location.href.indexOf('://www.tumblr.com/dashboard') === -1) {
-				movesidebar = false;
+				return;
 			}
 		} else {
 			var disallowedurls = ["://www.tumblr.com/explore", "://www.tumblr.com/search"];
-			disallowedurls.forEach(function(s) {
-				if (document.location.href.indexOf(s) !== -1) {
-					movesidebar = false;
+			for (var i = 0; i < disallowedurls.length; i++) {
+				if (document.location.href.indexOf(disallowedurls[i]) !== -1) {
+					return;
 				}
-			});
+			}
 		}
 		
-		if (movesidebar) {
-			var account = document.getElementById("account_button");
-			account.click();
-			var popover = document.getElementsByClassName("popover--account-popover")[0];
-			var sidebar = document.getElementById("right_column");
-			popover.childNodes[0].classList.add("old_sidebar");
-			sidebar.insertBefore(popover.childNodes[0], sidebar.firstChild);
-			account.style.display = "none";
-			// this needs to be delayed a second for some reason
-			window.setTimeout(function() {
-				document.querySelector(".tab_nav_account.active").click();
-			}, 1000);
-		}
+		var account = document.getElementById("account_button");
+		account.click();
+		var popover = document.getElementsByClassName("popover--account-popover")[0];
+		var sidebar = document.getElementById("right_column");
+		popover.childNodes[0].classList.add("old_sidebar");
+		sidebar.insertBefore(popover.childNodes[0], sidebar.firstChild);
+		account.style.display = "none";
+		// this needs to be delayed a second for some reason
+		window.setTimeout(function() {
+			document.querySelector(".tab_nav_account.active").click();
+		}, 1000);
 	},
 	
 	destroy: function() {
