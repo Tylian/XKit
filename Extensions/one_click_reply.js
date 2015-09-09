ddb9d1b6016e48fb33e92dfb84502bd368ab8f2a
BlackjackKent <tumblrthreadtracker@gmail.com>
2015-09-09 13:16:23 -0500

    Stop one-click-reply posts from dropping all linebreaks after being posted

--- Extensions/one_click_reply.js
+++ Extensions/one_click_reply.js
@@ -1,7 +1,7 @@
 //* TITLE One-Click Reply **//
-//* VERSION 2.0.5 **//
+//* VERSION 2.0.6 **//
 //* DESCRIPTION Lets you reply to notifications **//
-//* DEVELOPER STUDIOXENIX **//
+//* DEVELOPER new-xkit **//
 //* DETAILS To use this extension, hover over a notification and click on the Reply button. If Multi-Reply is on, hold down the ALT key while clicking on the Reply button to select/deselect posts and reply to all of them at once. **//
 //* FRAME false **//
 //* BETA false **//
@@ -637,7 +637,7 @@ XKit.extensions.one_click_reply = new Object({
 			if (tags) {
 				XKit.interface.post_window.add_tag(tags.split(","));
 			}
-			XKit.interface.post_window.set_content_html(m_sentence + "<br/>");
+			XKit.interface.post_window.set_content_html(m_sentence + "<p><br/></p>");
 			XKit.interface.post_window_listener.remove("one_click_reply_fill_post");
 			XKit.tools.set_setting("xkit_one_click_reply_data", "{}");
 		} catch(e) {
