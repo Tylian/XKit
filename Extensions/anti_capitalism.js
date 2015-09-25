23d99951dd59ba2f6f3fa31c055cec3dfb2969ff
ThePsionic <me@thepsionic.com>
2015-09-25 14:54:38 +0200

    whoopsie

--- Extensions/anti_capitalism.js
+++ Extensions/anti_capitalism.js
@@ -39,7 +39,7 @@ XKit.extensions.anti_capitalism = new Object({
 	    }
 
 		if (XKit.extensions.anti_capitalism.preferences.sponsored_posts.value) {
-			XKit.tools.add_css(" .post .sponsored_post {display:none}", "anti_capitalism_sponsored_posts");
+			XKit.tools.add_css(" .post.sponsored_post {display:none}", "anti_capitalism_sponsored_posts");
 		}
 
 	    if (XKit.extensions.anti_capitalism.preferences.sponsored_ads.value) {
