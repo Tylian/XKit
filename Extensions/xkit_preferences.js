98647c46d2a5aa5b70a9988fcc617e3c24d62d81
Bryan Tsang <bvtsang@gmail.com>
2015-10-01 23:41:20 -0700

    xkit_preferences: fix indentation

--- Extensions/xkit_preferences.js
+++ Extensions/xkit_preferences.js
@@ -688,7 +688,7 @@ XKit.extensions.xkit_preferences = new Object({
 
 				XKit.storage.set("xkit_preferences","festivus","no");
 
-        stopFestivusInterval();
+				stopFestivusInterval();
 			} else {
 
 				$("#xkit-festivus-toggle").html(festivus_off);
@@ -696,42 +696,42 @@ XKit.extensions.xkit_preferences = new Object({
 
 				XKit.storage.set("xkit_preferences","festivus","yes");
 
-        startFestivusInterval();
+				startFestivusInterval();
 			}
 
 		});
 
 		// $("#xkit-festivus").css("background-image","url('" + XKit.extensions.xkit_preferences.festivus_lights + "')");
 
-    function stopFestivusInterval() {
-      clearInterval(XKit.extensions.xkit_preferences.festivus_lights_interval);
-    }
+		function stopFestivusInterval() {
+			clearInterval(XKit.extensions.xkit_preferences.festivus_lights_interval);
+		}
 
-    function startFestivusInterval() {
-      stopFestivusInterval();
+		function startFestivusInterval() {
+			stopFestivusInterval();
 
-      XKit.extensions.xkit_preferences.festivus_lights_interval = setInterval(function() {
+			XKit.extensions.xkit_preferences.festivus_lights_interval = setInterval(function() {
 
-        if ($("#xkit-festivus").attr('data-id-light') === "1") {
-          $("#xkit-festivus").css("background-position","0px 33px");
-          $("#xkit-festivus").attr('data-id-light', "2");
-          return;
-        }
+				if ($("#xkit-festivus").attr('data-id-light') === "1") {
+					$("#xkit-festivus").css("background-position","0px 33px");
+					$("#xkit-festivus").attr('data-id-light', "2");
+					return;
+				}
 
-        if ($("#xkit-festivus").attr('data-id-light') === "2") {
-          $("#xkit-festivus").css("background-position","0px 66px");
-          $("#xkit-festivus").attr('data-id-light', "3");
-          return;
-        }
+				if ($("#xkit-festivus").attr('data-id-light') === "2") {
+					$("#xkit-festivus").css("background-position","0px 66px");
+					$("#xkit-festivus").attr('data-id-light', "3");
+					return;
+				}
 
-        if ($("#xkit-festivus").attr('data-id-light') === "3") {
-          $("#xkit-festivus").css("background-position","0px 0px");
-          $("#xkit-festivus").attr('data-id-light', "1");
-          return;
-        }
+				if ($("#xkit-festivus").attr('data-id-light') === "3") {
+					$("#xkit-festivus").css("background-position","0px 0px");
+					$("#xkit-festivus").attr('data-id-light', "1");
+					return;
+				}
 
-      }, 1000);
-    }
+			}, 1000);
+		}
 
 		if (XKit.extensions.xkit_preferences.hide_xcloud_if_not_installed === true) {
 			if (XKit.installed.check("xcloud") === false) {
