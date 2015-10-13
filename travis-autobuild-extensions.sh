ed8bd4940eca259d746f300c983482e7d8348d6c
Wolvan <wolvan1@gmail.com>
2015-10-13 15:35:45 +0200

    Make travis build fail if extension dist rebuild fails

--- travis-autobuild-extensions.sh
+++ travis-autobuild-extensions.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+set -e
+if [ "$TRAVIS_REPO_SLUG" == "$GH_REPO" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo -e "Rebuilding extension dist...\n"
 
-if [ "$TRAVIS_REPO_SLUG" == "new-xkit/XKit" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo -e "Rebuilding extension dist...\n" &&\
+  git config --global user.email "travis@travis-ci.org" || true
+  git config --global user.name "travis-ci" || true
+  git remote set-branches --add origin gh-pages
+  git fetch
+  git checkout -f -t -b gh-pages origin/gh-pages
+  git merge master -m "Merge master $TRAVIS_COMMIT"
+  gulp build:extensions
+  gulp build:themes
+  git add Extensions
+  git diff --staged --quiet Extensions || {
+    git commit -m "Rebuild distribution based on $TRAVIS_COMMIT"
+	git push -fq https://"$GH_TOKEN"@github.com/"$GH_REPO" gh-pages > /dev/null
+	echo -e "Rebuilt extension dist.\n"
+	exit 0
+  }
+  echo -e "Nothing to commit, extension dist has not changed."
 
-  cd $HOME &&\
-  git config --global user.email "travis@travis-ci.org" &&\
-  git config --global user.name "travis-ci" &&\
-  git clone https://"$GH_TOKEN"@github.com/new-xkit/XKit &&\
-  cd XKit &&\
-  git checkout gh-pages &&\
-  git merge master -m "Merge master $TRAVIS_COMMIT" &&\
-  npm install -g gulp &&\
-  npm install &&\
-  gulp build:extensions &&\
-  gulp build:themes &&\
-  git add Extensions &&\
-  git commit -m "Rebuild distribution based on $TRAVIS_COMMIT" &&\
-  git push -fq origin gh-pages > /dev/null &&\
-
-  echo -e "Rebuilt extension dist.\n"
 fi
