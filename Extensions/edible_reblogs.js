3ce00a4d6ae333a08434216f4a0e9e6ced5decaa
nepeat <nepeat@users.noreply.github.com>
2015-10-20 04:04:19 -0800

    I'm sorry.
    Squashed 3 additional commits
    
    * Bump version of edible_reblogs.js
    * Checkbox toggle to toggle the torturous bread.
    * Remove the === true check for the bread hell.

--- Extensions/edible_reblogs.js
+++ Extensions/edible_reblogs.js
@@ -1,7 +1,7 @@
 //* TITLE Edible Reblogs **//
-//* VERSION 1.0.2 **//
+//* VERSION 1.0.3 **//
 //* DESCRIPTION We are serious developers... **//
-//* DETAILS ...with our priorities in order. Puts bread in the background of each post. **//
+//* DETAILS ...with our priorities in order. Puts bread in literally everything. **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
 //* BETA false **//
@@ -10,14 +10,33 @@ XKit.extensions.edible_reblogs = new Object({
 
 	running: false,
 
+	preferences: {
+		bread_hell: {
+			text: 'Make everything literally bread.',
+			default: false,
+			value: false
+		}
+	},
+
 	run: function() {
 		this.running = true;
 		XKit.tools.init_css("edible_reblogs");
+
+		// "Dear god :fearful:"
+		if (XKit.extensions.edible_reblogs.preferences.bread_hell.value) {
+			XKit.post_listener.add("my_extension", XKit.extensions.edible_reblogs.breadify);
+			XKit.extensions.edible_reblogs.breadify();
+		}
+	},
+
+	breadify: function() {
+		$("*:not(.bread)").addClass("bread");
 	},
 
 	destroy: function() {
 		XKit.tools.remove_css("edible_reblogs");
 		this.running = false;
+		$("*").removeClass("bread");
 	}
 
 });
