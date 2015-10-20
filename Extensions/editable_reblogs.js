c7f7814ce54b10ecf6b67e28c6b904342a103a2f
dlmarquis <dmarquis@g.hmc.edu>
2015-10-20 06:06:47 -0400

    Fix instance of previous comment being quote

--- Extensions/editable_reblogs.js
+++ Extensions/editable_reblogs.js
@@ -1,5 +1,5 @@
 //* TITLE Editable Reblogs **//
-//* VERSION 2.1.3 **//
+//* VERSION 2.1.4 **//
 //* DESCRIPTION	Restores ability to edit previous reblogs of a post **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
@@ -61,12 +61,7 @@ XKit.extensions.editable_reblogs = new Object({
 			});
 			all_quotes.forEach(function(data, index, all) {
 				var reblog_content = data.reblog_content.replace("tmblr-truncated read_more_container", "");
-				//don't wrap if the previous user didn't add a comment
-				if (reblog_content.indexOf("</blockquote>", reblog_content.length - 13) !== -1 || reblog_content.length === 0) {
-					all_quotes_text = reblog_content;
-				} else {
-					all_quotes_text = "<p><a class='tumblr_blog' href='" + data.reblog_url + "'>" + data.reblog_author + "</a>:</p><blockquote>" + all_quotes_text + reblog_content + "</blockquote>";
-				}
+				all_quotes_text = "<p><a class='tumblr_blog' href='" + data.reblog_url + "'>" + data.reblog_author + "</a>:</p><blockquote>" + all_quotes_text + reblog_content + "</blockquote>";
 			});
 		}
 		try {
