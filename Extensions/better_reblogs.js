0232097203c1d21a9a8fdfce3cc78eccf03d385a
evan <eg1290@gmail.com>
2015-10-25 14:29:28 -0400

    Make reblog non-tree contributed content handling more robust. Fixes #644
    Sometimes, there will be reblog "contributed content" (from the most
    recent user) without there being a reblog tree. an example for this is
    the first reblog of an ask with an answer, a reblog with text content
    of a media post that didn't have text content before, or (I think)
    a text post where previous reblogs were removed. This makes their
    handeling more robust.

--- Extensions/better_reblogs.js
+++ Extensions/better_reblogs.js
@@ -1,5 +1,5 @@
 //* TITLE Reblog Display Options **//
-//* VERSION 1.1.1 **//
+//* VERSION 1.1.2 **//
 //* DESCRIPTION Adds different styles to the new reblog layout, including the "classic" nested look. **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
@@ -302,11 +302,10 @@ XKit.extensions.better_reblogs = new Object({
             var title = reblog_tree.find('.reblog-title').clone();
 
             if (!reblog_tree.length){
-                // this shouldn't happen, but does sometimes on broken posts
                 var content = $this.find(".reblog-list-item.contributed-content .reblog-content").clone();
                 if(content.length){
-                    content.addClass("post_body"); // sure why not.
-                    $this.find(".post_container").append(content);
+                    content.addClass("post_body");
+                    $this.find(".reblog-list-item.contributed-content").before(content);
                 }
                 return;
             }
