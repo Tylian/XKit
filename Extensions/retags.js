ef0f877e5c80d56f1c7c27869b6bbcd7a675e1e5
David McLean <gopsychonauts@gmail.com>
2015-11-04 00:00:58 +1100

    retags: bump version

--- Extensions/retags.js
+++ Extensions/retags.js
@@ -1,6 +1,6 @@
 //* TITLE       Retags **//
 //* DEVELOPER   new-xkit **//
-//* VERSION     1.0.4 **//
+//* VERSION     1.0.5 **//
 //* DESCRIPTION Adds tags to reblog notes **//
 //* FRAME       false **//
 //* SLOW        false **//
