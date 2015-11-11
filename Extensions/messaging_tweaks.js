bbb13ce93b9e9b92da9ece2d0c665b4064b657f3
Liz Silich <lsilich04@gmail.com>
2015-11-11 17:17:16 -0600

    Adding ability to change text colors
    Changes in lines 65-76, 187-194

--- Extensions/messaging_tweaks.js
+++ Extensions/messaging_tweaks.js
@@ -62,6 +62,18 @@ XKit.extensions.messaging_tweaks = new Object({
 			default: "rgba(0,0,0,.1)",
 			value: "rgba(0,0,0,.1)"
 		},
+		"my_chat_bubble_text": {
+			text: "Text color for my chat bubbles",
+			type: "text",
+			default: "rgb(68,68,68)",
+			value: "rgb(68,68,68)"
+		},
+		"other_chat_bubble_text": {
+			text: "Text color for other people's chat bubbles",
+			type: "text",
+			default: "rgb(68,68,68)",
+			value: "rgb(68,68,68)"
+		},
 		"background_override": {
 			text: "Change chat background color",
 			type: "text",
@@ -174,6 +186,12 @@ XKit.extensions.messaging_tweaks = new Object({
 		
 		XKit.tools.add_css(".messaging-conversation .xkit-others_messaging_message .conversation-message-text .message-bubble { background-color: " + XKit.extensions.messaging_tweaks.preferences.other_chat_bubble_background.value + " !important; }", "messaging_tweaks");
 		XKit.tools.add_css(".messaging-conversation .xkit-my_messaging_message .conversation-message-text .message-bubble { background-color: " + XKit.extensions.messaging_tweaks.preferences.my_chat_bubble_background.value + " !important; }", "messaging_tweaks");
+		
+		XKit.tools.add_css(".messaging-conversation .xkit-others_messaging_message .conversation-message-text .message-bubble { color: " + XKit.extensions.messaging_tweaks.preferences.other_chat_bubble_text.value + " !important; }", "messaging_tweaks");
+		XKit.tools.add_css(".messaging-conversation .xkit-my_messaging_message .conversation-message-text .message-bubble { color: " + XKit.extensions.messaging_tweaks.preferences.my_chat_bubble_text.value + " !important; }", "messaging_tweaks");
+		
+		XKit.tools.add_css(".messaging-conversation .xkit-others_messaging_message .conversation-message-text .message-bubble-header a { color: " + XKit.extensions.messaging_tweaks.preferences.other_chat_bubble_text.value + " !important; }", "messaging_tweaks");
+		XKit.tools.add_css(".messaging-conversation .xkit-my_messaging_message .conversation-message-text .message-bubble-header a { color: " + XKit.extensions.messaging_tweaks.preferences.my_chat_bubble_text.value + " !important; }", "messaging_tweaks");
 		
 		// There's either 1 or no messaging-conversation-popovers on extension start
 		$(".messaging-conversation-popovers").each(function() {
