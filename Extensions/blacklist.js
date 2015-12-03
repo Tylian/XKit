4e018acdc76a557f32bb5170e1db611f81b812bc
wintergone <whysosilent@gmail.com>
2015-12-03 23:43:02 +0000

    Update blacklist.js

--- Extensions/blacklist.js
+++ Extensions/blacklist.js
@@ -1,5 +1,5 @@
 //* TITLE Blacklist **//
-//* VERSION 2.7.6 **//
+//* VERSION 2.7.7 **//
 //* DESCRIPTION Clean your dash **//
 //* DETAILS This extension allows you to block posts based on the words you specify. If a post has the text you've written in the post itself or it's tags, it will be replaced by a warning, or won't be shown on your dashboard, depending on your settings. **//
 //* DEVELOPER new-xkit **//
