d30513e483d723f16134a5c32c7a6dc862e4b952
dlmarquis <dmarquis@g.hmc.edu>
2016-01-07 20:23:48 -0500

    Version bump

--- Extensions/classic_header.js
+++ Extensions/classic_header.js
@@ -1,5 +1,5 @@
 //* TITLE Header Options **//
-//* VERSION 2.4.0 **//
+//* VERSION 2.4.1 **//
 //* DESCRIPTION Customize the header. **//
 //* DEVELOPER new-xkit **//
 //* DETAILS This extension adds your blogs on the top of the page, so you can easily switch between blogs. The blog limit on the header is five, but you can limit this to three blogs and turn off the blog title bubble from the settings. **//
