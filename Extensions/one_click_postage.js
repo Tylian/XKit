2d5421a1b94275f13fa01696fa8cfabb9e5324ac
Jeremie <jeremie.bonal@gmail.com>
2016-05-16 21:27:46 +0200

    Bumped version.

--- Extensions/one_click_postage.js
+++ Extensions/one_click_postage.js
@@ -1,5 +1,5 @@
 //* TITLE One-Click Postage **//
-//* VERSION 4.2.0 **//
+//* VERSION 4.3.0 **//
 //* DESCRIPTION Lets you easily reblog, draft and queue posts **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
