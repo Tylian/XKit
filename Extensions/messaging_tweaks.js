9d2c256167b9dae701ba8b885666125c6d2fe50c
Wolvan <wolvan1@gmail.com>
2016-06-28 17:27:12 +0200

    Change option in messaging tweaks to hide IM part in share menu
    Since the paper plane is not only IM anymore, the tweak now only hides
    the IM part of the share menu. The Hide Share Button tweak in Tweaks has
    been fixed to fully hide the paper plane

--- Extensions/messaging_tweaks.js
+++ Extensions/messaging_tweaks.js
@@ -36,7 +36,7 @@ XKit.extensions.messaging_tweaks = new Object({
 			value: false
 		},
 		"hide_send_post": {
-			text: "Hide the send post paper plane on posts",
+			text: "Hide the send post through IM in the post share menu",
 			default: false,
 			value: false
 		},
@@ -364,7 +364,7 @@ XKit.extensions.messaging_tweaks = new Object({
 			XKit.tools.add_css(".conversation-message-text .message-bubble-header a {display:none;}", "messaging_tweaks");
 		}
 		if (XKit.extensions.messaging_tweaks.preferences.hide_send_post.value) {
-			XKit.tools.add_css(".post_control.share {display:none;}", "messaging_tweaks");
+			XKit.tools.add_css(".messaging-share-post-search, .messaging-share-post-main {display:none;}", "messaging_tweaks");
 		}
 		if (XKit.extensions.messaging_tweaks.preferences.make_icons_round.value) {
 			XKit.tools.add_css(".avatar > img { border-radius: 30px !important; transition: border-radius 0.5s; }", "messaging_tweaks");
