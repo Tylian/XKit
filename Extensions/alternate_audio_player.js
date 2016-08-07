a609c2f309db1d0d83011e39cc10d2ef1e0d8c98
Aku no Homu <lily.tolstoy@gmail.com>
2016-08-06 20:18:14 -0700

    Add semicolons

--- Extensions/alternate_audio_player.js
+++ Extensions/alternate_audio_player.js
@@ -28,7 +28,7 @@ XKit.extensions.alternate_audio_player = new Object({
 
 	run: function() {
 		"use strict";
-		this.running = true
+		this.running = true;
 		XKit.post_listener.add("alternate_audio_player", this.add_audio);
 		this.add_audio();
 	},
@@ -36,24 +36,24 @@ XKit.extensions.alternate_audio_player = new Object({
 	add_audio: function() {
 		"use strict";
 		jQuery(".audio_player_container").not(".xkit-audio-added").each(function(index) {
-			const ob = jQuery(this)
-			ob.addClass("xkit-audio-added")
-			const url = ob.attr("data-stream-url")
-			const key = ob.attr("data-post-key")
+			const ob = jQuery(this);
+			ob.addClass("xkit-audio-added");
+			const url = ob.attr("data-stream-url");
+			const key = ob.attr("data-post-key");
 			if (typeof url !== "undefined" && typeof key !== "undefined") {
 				const player = jQuery("<audio class='xkit-audio-player' src='" + url + "?play_key=" + key +
 					"' preload='none' style='width:100%;margin-top:8px;' controls><p>No audio support detected</p></audio>")
 				player.prop("volume", XKit.extensions.alternate_audio_player.preferences.default_volume.value);
-				ob.parent().append(player)
+				ob.parent().append(player);
 			}
 		})
 	},
 
 	destroy: function() {
 		"use strict";
-		XKit.post_listener.remove("alternate_audio_player")
-		jQuery(".xkit-audio-added").removeClass("xkit-audio-added")
-		jQuery(".xkit-audio-player").remove()
+		XKit.post_listener.remove("alternate_audio_player");
+		jQuery(".xkit-audio-added").removeClass("xkit-audio-added");
+		jQuery(".xkit-audio-player").remove();
 		this.running = false;
 	}
 
