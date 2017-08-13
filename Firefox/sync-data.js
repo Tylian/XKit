7a9581c156a8d04493a12eeac0bd211e89ec737d
James Hobin <hobinjk@mit.edu>
2017-08-13 09:22:10 -0700

    Asynchronously migrate extension data (#1385)
    * Asynchronously migrate extension data
    
    * Add storage migration warning; Default to unlimited storage
    
    * Bump version to 7.7.6-less-broken
    
    * Skip update if current value is longer
    
    * copying -> migrating for clarity
    
    * Bump version
    
    * Excise misleading comment

--- Firefox/sync-data.js
+++ Firefox/sync-data.js
@@ -1,16 +1,44 @@
-/* globals require, exports */
+/* globals require, exports, Promise */
 let prefs = require('sdk/preferences/service');
+let {setTimeout} = require('sdk/timers');
 let prefRoot = 'extensions.xkit7.';
+
+function migratePreference(port, prefKey) {
+	let name = prefKey.substring(prefRoot.length);
+	let value = prefs.get(prefKey, null);
+	let msg = {};
+	msg[name] = value;
+	// postMessage doesn't return a promise even though it could :(
+	port.postMessage(msg);
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			if (port.error) {
+				reject(port.error);
+			} else {
+				resolve();
+			}
+		}, 10);
+	});
+}
+
+function migratePreferences(port, existingKeys) {
+	if (existingKeys.length === 0) {
+		return Promise.resolve();
+	}
+	let key = existingKeys.pop();
+	return migratePreference(port, key).then(function() {
+		return migratePreferences(port, existingKeys);
+	});
+}
 
 exports.setSyncLegacyDataPort = function(port) {
 
-	// Get all and broadcast to webext
+	// Get all and broadcast to webext one at a time
 	let existingKeys = prefs.keys(prefRoot);
-	let xkitStorage = {};
-	for (let properName of existingKeys) {
-		let name = properName.substring(prefRoot.length);
-		let value = prefs.get(properName, null);
-		xkitStorage[name] = value;
-	}
-	port.postMessage(xkitStorage);
+
+	migratePreferences(port, existingKeys).then(() => {
+		return port.postMessage({isProperlyMigrated: true});
+	}).catch(err => {
+		console.error('preferences not migrated', err);
+	});
 };
