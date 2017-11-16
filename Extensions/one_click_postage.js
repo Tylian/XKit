1dd2b3cb6465f966dbde4bd48205c2665ec15eac
Xiao Di Guan <xdg@puxlit.net>
2017-11-16 16:30:25 +1100

    Bump OCP version again

--- Extensions/one_click_postage.js
+++ Extensions/one_click_postage.js
@@ -1,5 +1,5 @@
 //* TITLE One-Click Postage **//
-//* VERSION 4.3.6 **//
+//* VERSION 4.3.7 **//
 //* DESCRIPTION Lets you easily reblog, draft and queue posts **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
