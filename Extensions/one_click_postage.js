ad83a2a7dab15872c0fb77c9cc7ad90fbf3ddc73
James Hobin <hobinjk@gmail.com>
2018-01-05 12:45:19 -0500

    Bump One Click Postage version to 4.3.8
    Since I didn't give @puxlit any time to bump the version themselves.
    This is what I get for trying to do software development during my lunch
    break from software development
    
    Follow-up to #1459

--- Extensions/one_click_postage.js
+++ Extensions/one_click_postage.js
@@ -1,5 +1,5 @@
 //* TITLE One-Click Postage **//
-//* VERSION 4.3.7 **//
+//* VERSION 4.3.8 **//
 //* DESCRIPTION Lets you easily reblog, draft and queue posts **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
