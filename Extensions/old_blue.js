088df4b6971f2bc26f5e59215b929b19c2559094
April Sylph <april@sylph.tech>
2019-02-22 02:50:47 +0000

    [Old Blue] Add global, index.build rules (#1662)
    * Add index.build.css
    
    * Actually do global.css
    
    * Bump version to 1.0.0
    
    * Add details, link to css file
    
    * Clarify CSS file link

--- Extensions/old_blue.js
+++ Extensions/old_blue.js
@@ -1,6 +1,7 @@
 //* TITLE Old Blue **//
-//* VERSION 0.1.1 **//
+//* VERSION 1.0.0 **//
 //* DESCRIPTION No more dark blue background! **//
+//* DETAILS Reverses the so-called accessibility update that causes so many headaches. **//
 //* DEVELOPER New-XKit **//
 //* FRAME false **//
 //* BETA false **//
@@ -8,6 +9,15 @@
 XKit.extensions.old_blue = new Object({
 
 	running: false,
+
+	preferences: {
+		"note": {
+			text: `If you would like to use this as a userstyle in a faster-loading add-on, the CSS this extension uses can be found <a href="https://raw.githubusercontent.com/new-xkit/XKit/master/Extensions/old_blue.css" target="_blank">here</a>.`,
+			type: "separator"
+		}
+	},
+
+	cpanel: () => $(".xkit-extension-setting-separator").css("margin", 0).css("text-transform", "none"),
 
 	run: function() {
 		this.running = true;
